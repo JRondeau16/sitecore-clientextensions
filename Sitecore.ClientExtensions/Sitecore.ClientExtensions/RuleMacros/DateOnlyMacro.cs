b5abdc41a17a63a9004291d21ac61407940108e3
Adam Conn <adam.conn@gmail.com>
2013-11-04 16:44:36 -0800

    added date macros for rule editor

---
+++ Sitecore.ClientExtensions/Sitecore.ClientExtensions/RuleMacros/DateOnlyMacro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Text;
+
+namespace Sitecore.ClientExtensions.RuleMacros
+{
+    public class DateOnlyMacro : DateTimeMacroEx
+    {
+        protected override UrlString GetDialogUrl(string value)
+        {
+            var url = base.GetDialogUrl(value);
+            url["time"] = "false";
+            return url;
+        }
+
+        protected override string GetWidth()
+        {
+            return "300px";
+        }
+    }
+}
